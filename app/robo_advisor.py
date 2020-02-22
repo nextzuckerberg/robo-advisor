f9e6db757a0e0026ce337e0399d2298605a8aa36
nextzuckerberg <ac1895@georgetown.edu>
2020-02-22 11:41:16 -0500

    Writing example data to CSV

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -2,6 +2,7 @@
 
 import json
 import csv
+import os
 
 import requests
 
@@ -63,7 +64,15 @@ recent_high = max(high_prices)
 
 
  
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices_sales.csv")
 
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
 
 
 print("-------------------------")
@@ -80,18 +89,11 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
-print("WRITING DATA TO CSV")
+print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
 
 
-csv_file_path = "data/prices.csv" # a relative filepath
 
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
-    writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
