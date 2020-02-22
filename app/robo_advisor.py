4f771c14f5cb41f481c5a5f9150846fd7be969b0
nextzuckerberg <ac1895@georgetown.edu>
2020-02-22 10:50:06 -0500

    Getting Latest closing price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,7 +3,15 @@
 import requests
 import json
 
-
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
 
 
 
@@ -17,6 +25,7 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
+latest_closing = parsed_response["Time Series (Daily)"]["2020-02-21"]["4. close"]
 
 #breakpoint()
 
@@ -35,7 +44,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_closing))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
