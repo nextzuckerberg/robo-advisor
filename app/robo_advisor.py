95dbb138ebacd3d8d8ae38f47b94cffdfa96e11d
nextzuckerberg <ac1895@georgetown.edu>
2020-02-22 11:20:49 -0500

    Getting the recent high price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -26,13 +26,11 @@ parsed_response = json.loads(response.text)
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
 
-
-
-
 tsd = parsed_response["Time Series (Daily)"]
 
 
 dates = list(tsd.keys())
+
 latest_day = dates[0] #maybe sort dates later
 
 
@@ -40,6 +38,15 @@ latest_day = dates[0] #maybe sort dates later
 
 
 latest_closing = tsd[latest_day]["4. close"]
+
+high_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float((high_price)))
+
+
+recent_high = max(high_prices)
 
 
 
@@ -59,7 +66,7 @@ print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_closing))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
